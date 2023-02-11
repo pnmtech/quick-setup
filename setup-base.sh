4f3793a4c3bd7601a980681a3b70c14b1206cff2
Citizen T <79518458+pnmtech@users.noreply.github.com>
2023-02-11 20:36:25 +0000

    Update setup-base.sh

--- setup-base.sh
+++ setup-base.sh
@@ -2,4 +2,5 @@
 sudo sh -c 'echo "fastestmirror=True" >>/etc/dnf/dnf.conf'
 sudo sh -c 'echo "max_parallel_downloads=10" >>/etc/dnf/dnf.conf'
 sudo sh -c 'echo "defaultyes=True" >>/etc/dnf/dnf.conf'
+sudo dnf install android-tools
 GRUB_CMDLINE_LINUX="mitigations=off"
