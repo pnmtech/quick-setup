1c9908a18f41e66772e96a1fdd165041f791ff45
Citizen T <79518458+pnmtech@users.noreply.github.com>
2023-02-11 20:26:07 +0000

    feature and rename

--- setup-base.sh
+++ setup-base.sh
@@ -2,3 +2,4 @@
 sudo sh -c 'echo "fastestmirror=True" >>/etc/dnf/dnf.conf'
 sudo sh -c 'echo "max_parallel_downloads=10" >>/etc/dnf/dnf.conf'
 sudo sh -c 'echo "defaultyes=True" >>/etc/dnf/dnf.conf'
+GRUB_CMDLINE_LINUX="mitigations=off"
