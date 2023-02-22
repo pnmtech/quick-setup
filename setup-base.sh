288d5ca48e4b238648ebd528b7ccb8afe973e8df
Citizen T <79518458+pnmtech@users.noreply.github.com>
2023-02-22 13:13:52 +0000

    Delete setup-base.sh

--- setup-base.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sudo bash
-sudo sh -c 'echo "fastestmirror=True" >>/etc/dnf/dnf.conf'
-sudo sh -c 'echo "max_parallel_downloads=10" >>/etc/dnf/dnf.conf'
-sudo sh -c 'echo "defaultyes=True" >>/etc/dnf/dnf.conf'
-sudo dnf install android-tools
-GRUB_CMDLINE_LINUX="mitigations=off"
