83f1438b988e660c0fc82bd355e27c0d329a88f8
Citizen T <79518458+pnmtech@users.noreply.github.com>
2023-03-09 12:35:41 +0000

    Update setup-base.sh

--- setup-base.sh
+++ setup-base.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sudo bash
-sudo dnf install zsh
+sudo dnf install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo sh -c 'echo "fastestmirror=True" >>/etc/dnf/dnf.conf'
 sudo sh -c 'echo "max_parallel_downloads=10" >>/etc/dnf/dnf.conf'
